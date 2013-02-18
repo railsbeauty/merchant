2a3f8aee9e3e505e4f49a57baa22736fdb59599c
vinay <vinay@vertisinfotech.com>
2013-02-18 13:32:41 +0530

    create basic project setup

---
+++ merchant/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Merchant::Application.config.secret_token = 'ecafb6382627065533d025b800580a8885a722c1edfb497eb6590581c240d904f78ff55665c86c9a980ca45db8a346db72167ec31d00b570c033e3f1a871cdc4'
